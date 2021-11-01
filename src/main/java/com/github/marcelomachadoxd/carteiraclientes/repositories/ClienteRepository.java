c67b7681a9bcc07534dae9da919bd66e510f8731
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-11-01 13:44:41 -0300

    repositories

---
+++ src/main/java/com/github/marcelomachadoxd/carteiraclientes/repositories/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.github.marcelomachadoxd.carteiraclientes.repositories;
+
+import com.github.marcelomachadoxd.carteiraclientes.entities.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
