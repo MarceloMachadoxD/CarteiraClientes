d8ce01907c0ea1fae5eccf0db995d4884a826f46
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-11-01 13:43:17 -0300

    Initial commit

---
+++ src/main/java/com/github/marcelomachadoxd/carteiraclientes/CarteiraClientesApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcelomachadoxd.carteiraclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraClientesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarteiraClientesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/marcelomachadoxd/carteiraclientes/CarteiraClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcelomachadoxd.carteiraclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraClientesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
