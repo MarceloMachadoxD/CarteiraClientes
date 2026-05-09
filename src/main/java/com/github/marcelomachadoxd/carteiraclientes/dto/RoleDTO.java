9b4689a3a30b4d456eb250c68efc4cd25f3db6ca
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2026-05-09 19:04:10 -0300

    5.2 Adicionar @Schema(example = ...) nos campos de RoleDTO

--- src/main/java/com/github/marcelomachadoxd/carteiraclientes/dto/RoleDTO.java
+++ src/main/java/com/github/marcelomachadoxd/carteiraclientes/dto/RoleDTO.java
@@ -1,10 +1,14 @@
 package com.github.marcelomachadoxd.carteiraclientes.dto;
 
 import com.github.marcelomachadoxd.carteiraclientes.entities.Role;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public class RoleDTO {
 
+    @Schema(example = "1")
     private Long id;
+
+    @Schema(example = "ROLE_CORRETOR")
     private String nome;
 
     public RoleDTO() {
